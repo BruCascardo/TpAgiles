f6d4f18e7ae047be080a9fb58dac5c90794aa1cd
Maximiliano Aguilar <maximilianourielaguilar@gmail.com>
2026-07-03 21:00:51 -0300

    GREEN: feat(Ahorcado): logica para revelar letras correctas

--- src/domain/Ahorcado.ts
+++ src/domain/Ahorcado.ts
@@ -1,17 +1,21 @@
 export class Ahorcado {
   private palabraSecreta: string;
+  private adivinadas: Set<string> = new Set();
 
   constructor(palabra: string) {
     this.palabraSecreta = palabra;
   }
 
   adivinar(letra: string): void {
-    // Se implementará a partir de los tests unitarios
+    this.adivinadas.add(letra.toUpperCase());
   }
 
   palabraEnmascarada(): string {
-  return this.palabraSecreta.split('').map(() => '_').join(' ');
-}
+    return this.palabraSecreta
+      .split('')
+      .map(letra => this.adivinadas.has(letra.toUpperCase()) ? letra : '_')
+      .join(' ');
+  }
 
   vidas(): number {
     // Se implementará a partir de los tests unitarios
