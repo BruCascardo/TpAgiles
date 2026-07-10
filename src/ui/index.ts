0688750baa666a0ce59eef12f6f8a26bdf7b01be
Bruno Cascardo <brucascardo@gmail.com>
2026-07-09 23:12:54 -0300

    GREEN: feat(index): conecta el generador de palabras al azar en el arranque de la app

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,7 +2,13 @@ import { Ahorcado } from "../domain/Ahorcado";
 import { mountApp } from "./main";
 
 const params = new URLSearchParams(window.location.search);
-const palabra = params.get("word") || "GATO";
+
+// Definimos nuestro diccionario
+const diccionario = ["ALGORITMO", "TYPESCRIPT", "COMPUTADORA", "INTERNET", "PROGRAMACION", "DESARROLLO"];
+
+// Si viene por URL la usamos (útil para el resto de los ATs deterministas)
+// Si no, elegimos una al azar usando el método de dominio (sin pasar el seam, usa Math.random)
+const palabra = params.get("word") || Ahorcado.elegirPalabraAleatoria(diccionario);
 
 const juego = new Ahorcado(palabra);
 const appContainer = document.getElementById("app");
